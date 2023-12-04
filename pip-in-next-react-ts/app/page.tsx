dc6b99f258fbea36a327ec34998255311be39d18
SubhamSvelteTech <shubham.sahu@sveltetech.com>
2023-12-04 15:17:38 +0530

    .

---
+++ pip-in-next-react-ts/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>Custom Picture In Picture Mode</main>;
+}
---
+++ pip-in-next-react-ts/components/PIPComponent.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const PIPComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PIPComponent
